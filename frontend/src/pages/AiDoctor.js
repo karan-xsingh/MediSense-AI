07d56112881fb7b81cca6c3bd786c060f2350ac5
karan-xsingh <ksinghrathore28@gmail.com>
2026-06-25 16:03:01 +0530

    Fix API URL in pages folder

--- frontend/src/pages/AiDoctor.js
+++ frontend/src/pages/AiDoctor.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://karan18singh.pythonanywhere.com';
 
 function AiDoctor() {
   const navigate = useNavigate();
